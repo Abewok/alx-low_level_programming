6097633885504490bc805e3fae851ac81d363125
Abewok <abewok@gmail.com>
2022-09-13 20:29:15 +0300

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+ *                                                                                                                                      
+ * main - fibonachi.                                                                                                                    
+ *                                                                                                                                      
+ * Return: Always 0.                                                                                                                    
+ */                                                                                                                                     
+int main(void)                                                                                                                          
+{                                                                                                                                       
+        int i;                                                                                                                          
+ long int a, b, c;                                                                                                                    
+                                                                                                                                        
+        a = 1;                                                                                                                         
+        b = 2;                                                                                                                         
+        printf("1, ");                                                                                                                  
+        printf("2, ");                                                                                                                  
+        for (i = 0; i < 96; i++)                                                                                                        
+        {                                                                                                                               
+                c = b + a;                                                                                                            
+                b =a;                                                                                                                 
+                a = c;                                                                                                                 
+                printf("%ld", c);                                                                                                       
+                if (i != 95)                                                                                                            
+                {                                                                                                                       
+                        printf(", ");                                                                                                   
+                }                                                                                                                       
+        }                                                                                                                               
+        printf("\n");                                                                                                                   
+        return (0);                                                                                                                     
+} 
