a2ae390acab728caf2f1e5c8ccab7656712605d7
Abewok <abewok@gmail.com>
2022-09-13 15:56:26 +0300

    Create 0-putchar.c

---
+++ Create 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char T[10] = "BlenTedo\n";
+int i = 0;
+
+for (i = 0; i < 10; i++)
+{
+_putchar(T[i]);
+}
+return (0);
+}
+Footer
+© 2022 GitHub, Inc.
