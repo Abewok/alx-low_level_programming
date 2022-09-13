1e1f510dc601cc96031ef149a1bab3f8a51347df
Abewok <abewok@gmail.com>
2022-09-13 07:46:18 -0700

    l

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * main - check the code.
+ * 
+ *  Return: Always 0.
  */
 int main(void)
 {
@@ -46,4 +46,5 @@ printf(", ");
 }
 printf("\n");
 return (0);
+}
 }
