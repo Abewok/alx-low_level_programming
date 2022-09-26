723b36438316b6e9d2e760a081879082955a8011
Abewok <abewok@gmail.com>
2022-09-26 12:56:46 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,43 +1,22 @@
-#include "main.h"
-#include <stdio.h>
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- * Return: Nothing.
+#include "main.h"/**
+ * _memcpy - function that copies memory area.
+ *
+ * @dest: dest positions
+ * @src: source position
+ * @n: size of bytes
+ * Return: char
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-	unsigned int i;
 
-	i = 0;
-	while (i < size)
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (i < n)
 	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
+		*(dest + i) = *(src + j);
+		i += 1;
+		j += 1;
 	}
-	printf("\n");
-}
-/**
- * main - check the code
- * Return: Always 0.
- */
-int main(void)
-{
-	char buffer[98] = {0};
-	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-
-	simple_print_buffer(buffer, 98);
-	_memcpy(buffer + 50, buffer2, 10);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);    
-	return (0);
+	return (dest);
 }
